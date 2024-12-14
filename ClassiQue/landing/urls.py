5abafc105093ad9377710c19d2ce7e75c741e7bf
Samantha Laqueenna Ginting <167011131+sammmine@users.noreply.github.com>
2024-12-15 00:41:39 +0700

    refactor: upload buttons (to multiple file upload) and save to media direc in uploadPage.html

--- ClassiQue/landing/urls.py
+++ ClassiQue/landing/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('upload/', views.uploadPage, name='uploadPage'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ClassiQue/landing/views.py
+++ ClassiQue/landing/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, redirect
+import os
+from django.conf import settings
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 
@@ -6,4 +10,35 @@ def home(request): # Landing page
     return render(request, "landing/home.html")
 
 def uploadPage(request): # Path menuju halaman upload datasets
+    if request.method == 'POST':
+        # 1. Proses dataset audio
+        audio_files = request.FILES.getlist('audio-dataset')  # Input audio
+        if audio_files:
+            folder_audio = os.path.join(settings.MEDIA_ROOT, 'audio')
+            os.makedirs(folder_audio, exist_ok=True)
+            for file in audio_files:
+                fs = FileSystemStorage(location=folder_audio)
+                saved_path = fs.save(file.name, file)
+                print(f"Audio file saved to: {os.path.join(folder_audio, saved_path)}")
+
+        # 2. Proses dataset gambar
+        image_files = request.FILES.getlist('image-dataset')  # Input gambar
+        if image_files:
+            folder_image = os.path.join(settings.MEDIA_ROOT, 'images')
+            os.makedirs(folder_image, exist_ok=True)
+            for file in image_files:
+                fs = FileSystemStorage(location=folder_image)
+                saved_path = fs.save(file.name, file)
+                print(f"Image file saved to: {os.path.join(folder_image, saved_path)}")
+
+        # 3. Proses file mapper
+        mapper_file = request.FILES.get('mapper-data')  # Input mapper
+        if mapper_file:
+            folder_mapper = os.path.join(settings.MEDIA_ROOT, 'mapper')
+            os.makedirs(folder_mapper, exist_ok=True)
+            fs = FileSystemStorage(location=folder_mapper)
+            saved_path = fs.save(mapper_file.name, mapper_file)
+            print(f"Mapper file saved to: {os.path.join(folder_mapper, saved_path)}")
+
+        return render(request, "query/queryPage.html")
     return render(request, "landing/uploadPage.html")
