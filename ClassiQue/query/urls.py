c49cdf707938b65b519acdcf00112dba02e6afec
Samantha Laqueenna Ginting <167011131+sammmine@users.noreply.github.com>
2024-12-16 14:41:07 +0700

    fix: delete unnecessary folders

--- ClassiQue/query/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.queryPage, name="queryPage")
-]
